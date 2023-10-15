ceecd4743735d7bda04e35fcc9c585da7ff0a022
Muhammad Ahmad <122979965+Code-With-Ahmad@users.noreply.github.com>
2023-10-15 13:37:39 +0500

    Add files via upload

---
+++ Square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (void)
+{
+int val ;
+cout << "Enter Any Value To Find the square of that value" << endl;
+cin >> val;
+cout << "\n" << endl;
+cout << "-----------------------" << endl;
+cout << "\n" << endl;
+cout << "Your Answer is :-" << endl;
+cout << "\n" << endl;
+cout << val * val << endl;
+return 0;
+}
+
